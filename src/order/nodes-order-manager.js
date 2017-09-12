b310740a28e3b8047b8fdedec607a9575cd47fd7
fabiotestolin <fabio.testolin@iubenda.com>
2017-09-12 11:59:11 +0200

    more testing

--- src/order/nodes-order-manager.js
+++ src/order/nodes-order-manager.js
@@ -1,14 +1,9 @@
 class NodesOrderManager {
 
    constructor(nodes) {
-      this._maxPosition = 0;
+      this._maxPosition = -1;
       this._nodes = nodes;
-      this._nodes.forEach((n) => {
-         let position = n.getPosition();
-         if (this._maxPosition < position) {
-            this._maxPosition = position;
-         }
-      });
+      this._workOutMaxPositionBasedOnModelsData();
    }
 
    getMaxPosition() {
@@ -22,7 +17,31 @@ class NodesOrderManager {
    }
 
    removeNode(node) {
+      var newNodesList = [];
+      var positionOfTheNodeToRemove = node.getPosition();
 
+      this._nodes.forEach((current) => {
+         let position = current.getPosition();
+         if (node.getId() !== current.getId()) {
+            if (position > positionOfTheNodeToRemove) {
+               current.setPosition(position - 1);
+            }
+            newNodesList.push(current);
+         }
+      });
+
+      this._nodes = newNodesList;
+      this._workOutMaxPositionBasedOnModelsData();
+   }
+
+   _workOutMaxPositionBasedOnModelsData() {
+      this._maxPosition = 0;
+      this._nodes.forEach((n) => {
+         let position = n.getPosition();
+         if (this._maxPosition < position) {
+            this._maxPosition = position;
+         }
+      });
    }
 }
 
--- test/nodes-order-manager-spec.js
+++ test/nodes-order-manager-spec.js
@@ -6,19 +6,45 @@ var $ = require('jquery');
 describe('NodesOrderManager', function() {
 
    var manager, node1, node2, node3;
+   var newNode;
 
    beforeEach(function() {
 
       var eventHub = $({});
 
       node1 = new NodeView(1, "name1", {_iub_position: 1}, [], eventHub);
-      node2 = new NodeView(2, "name2", {_iub_position: 3}, [], eventHub);
-      node3 = new NodeView(3, "name3", {_iub_position: 2}, [], eventHub);
+      node2 = new NodeView(2, "name2", {_iub_position: 2}, [], eventHub);
+      node3 = new NodeView(3, "name3", {_iub_position: 3}, [], eventHub);
+
+      newNode = new NodeView(1, "name1", {}, [], eventHub);
 
       manager = new NodesOrderManager([node1, node2, node3]);
    });
 
-   it('Keeps track of the order assigned in the past', function() {
+   it('Keeps track of the order assigned to the nodes in the past', function() {
       assert.equal(3, manager.getMaxPosition());
+   });
+
+   it('Assigns a new coherent order to any new node', function() {
+      manager.addNode(newNode);
+      assert.equal(4, manager.getMaxPosition());
+      assert.equal(4, newNode.getPosition());
+   });
+
+   it('Re-works out the order of the nodes when a node is removed', function() {
+      manager.removeNode(node2);
+      assert.equal(2, manager.getMaxPosition());
+      assert.equal(1, node1.getPosition());
+      assert.equal(2, node3.getPosition());
+   });
+
+   it('Can removing and add nodes and consistenly keep track of the positions', function() {
+      manager.removeNode(node2);
+      manager.removeNode(node1);
+      assert.equal(1, manager.getMaxPosition());
+      assert.equal(1, node3.getPosition());
+      manager.addNode(newNode);
+      assert.equal(2, manager.getMaxPosition());
+      assert.equal(2, newNode.getPosition());
    });
 });
