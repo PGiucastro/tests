b179d492135863fabe1fdcfec9d907100fa1953c
fabiotestolin <fabio.testolin@iubenda.com>
2017-09-18 12:52:01 +0200

    blocked validation

--- src/view/main-view.js
+++ src/view/main-view.js
@@ -62,6 +62,8 @@ class MainView {
    _behaviour() {
 
       this._saveButton.click((e) => {
+         console.log(JSON.stringify(json, null, "   "));
+         return;
          for (var i = 0; i < this._nodeViews.length; i++) {
             let node = this._nodeViews[i];
             if (node.validate() === false) {
